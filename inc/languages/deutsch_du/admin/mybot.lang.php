a5b1d47f013f1bc956ca1780700fb6bbf2b8fb4f
JN-Jones <Jones_h1@web.de>
2012-07-05 11:26:40 +0200

    Initial Release. Added installation, settings and the acp module (not finished)

---
+++ inc/languages/deutsch_du/admin/mybot.lang.php
@@ -0,0 +1,46 @@
+<?php
+$l['mybot'] = "MyBot";
+$l['mybot_permission'] = "Kann MyBot konfigurieren?";
+
+
+/* Admin Modul */
+$l['mybot_overview'] = "Übersicht über alle Regeln";
+$l['mybot_addrule'] = "Neue Regel hinzufügen";
+$l['mybot_addrule_desc'] = "Hier kannst du eine neue Regel hinzufügen";
+$l['mybot_post'] = "Als Bot posten";
+$l['mybot_post_desc'] = "Hier kannst du als Bot posten";
+$l['mybot_update'] = "Update";
+$l['mybot_update_desc'] = "Hier kannst du MyBot updaten";
+
+
+/* Install Process */
+$l['mybot_installing'] = "Installiere MyBot";
+//$l['mybot_already_installed'] = "MyBot ist bereits installiert";
+$l['mybot_create_user'] = "Neuen Benutzer anlegen";
+$l['mybot_create_group'] = "Neue Gruppe anlegen";
+$l['mybot_user'] = "Benutzer";
+$l['mybot_user_desc'] = "Welcher Benutzer soll als Bot fungieren?";
+$l['mybot_user_not'] = "Interner Fehler [1]";
+$l['mybot_username'] = "Benutzername";
+$l['mybot_username_desc'] = "Wie soll der neue Benutzer heißen?";
+$l['mybot_username_not'] = "Kein Benutzername angegeben";
+$l['mybot_pw'] = "Passwort";
+$l['mybot_pw_desc'] = "Bitte gebe ein Passwort für den neuen Benutzer an";
+$l['mybot_pw_not'] = "Kein Passwort angegeben";
+$l['mybot_email'] = "E-Mail";
+$l['mybot_email_desc'] = "Bitte gebe die E-Mail für den neuen Benutzer an";
+$l['mybot_email_not'] = "Keine E-Mail angegeben";
+$l['mybot_group'] = "Gruppe";
+$l['mybot_group_desc'] = "Welcher Gruppe soll der neue Benutzer angehören?";
+$l['mybot_group_not'] = "Interner Fehler [2]";
+$l['mybot_groupname'] = "Gruppenname";
+$l['mybot_groupname_desc'] = "Wie soll die neue Gruppe heißen?";
+$l['mybot_groupname_not'] = "Kein Gruppenname angegeben";
+$l['mybot_save'] = "Speichern";
+$l['mybot_installed_group'] = "{1}<br />User \"{2}\" und Gruppe \"{3}\" neu hinzugefügt<br />Bitte bearbeite diese noch einmal zur Sicherheit";
+$l['mybot_installed_user'] = "{1}<br />User \"{2}\" neu hinzugefügt<br />Bitte bearbeite diesen noch einmal zur Sicherheit";
+$l['mybot_installed'] = "MyBot wurde erfolgreich installiert.";
+
+$l['mybot_pl_missing'] = "Die Installation konnte nicht gestartet werden, da die <b><a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a></b> fehlt";
+$l['mybot_pl_old'] = "Die Installation konnte nicht gestartet werden, da die <b><a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a></b> zu alt ist (Version 8 oder höher erforderlich)";
+?>
