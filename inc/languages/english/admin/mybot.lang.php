2dcd9009e5210ef3052bf9a764e729b9fd4cb53e
JN-Jones <Jones_h1@web.de>
2012-07-11 20:08:03 +0200

    Added english language file

---
+++ inc/languages/english/admin/mybot.lang.php
@@ -0,0 +1,69 @@
+<?php
+$l['mybot'] = "MyBot";
+$l['mybot_permission'] = "Can MyBot configure?";
+
+
+/* Admin Modul */
+$l['mybot_overview'] = "Overview of all rules";
+$l['mybot_addrule'] = "Add new rule";
+$l['mybot_addrule_desc'] = "Here you can add a new rule";
+$l['mybot_post'] = "Post as bot";
+$l['mybot_post_desc'] = "Here you can post as bot";
+$l['mybot_documentation'] = "Documentation";
+$l['mybot_documentation_desc'] = "Here you can see all variables, which you can use in various texts";
+
+/* Documentation */
+$l['mybot_variable'] = "Variable";
+$l['mybot_description'] = "Description";
+$l['mybot_global'] = "Global variables";
+$l['mybot_boardname'] = "The name of your Board";
+$l['mybot_botname'] = "The name of your bot";
+$l['mybot_register'] = "Variables for registration";
+$l['mybot_registered'] = "The name of the new registered";
+
+/* Post as Bot */
+$l['mybot_post_forum'] = "Forum";
+$l['mybot_post_forum_desc'] = "In which forum should be posted?";
+$l['mybot_post_forum_not'] = "Internal error [1]";
+$l['mybot_post_subject'] = "Title";
+$l['mybot_post_subject_desc'] = "The title of the thread";
+$l['mybot_post_subject_not'] = "No title given";
+$l['mybot_post_text'] = "Text";
+$l['mybot_post_text_desc'] = "The post which should be post. MyCode and smileys are enabled. <br />Note: MyBot variables are not available here";
+$l['mybot_post_text_not'] = "No text given";
+$l['mybot_post_submit'] = "Submit post";
+$l['mybot_post_category'] = "Your chosen forum is a category in which can't be posted.";
+$l['mybot_post_inserted'] = "Post saved";
+
+
+/* Install Process */
+$l['mybot_installing'] = "Install MyBot";
+//$l['mybot_already_installed'] = "MyBot ist bereits installiert";
+$l['mybot_create_user'] = "Create a new user";
+$l['mybot_create_group'] = "Create a new group";
+$l['mybot_user'] = "User";
+$l['mybot_user_desc'] = "Which user should act as bot?";
+$l['mybot_user_not'] = "Internal error [1]";
+$l['mybot_username'] = "Username";
+$l['mybot_username_desc'] = "How should the new user called?";
+$l['mybot_username_not'] = "No username given";
+$l['mybot_pw'] = "Password";
+$l['mybot_pw_desc'] = "Please enter a password for the new User";
+$l['mybot_pw_not'] = "No password given";
+$l['mybot_email'] = "E-Mail";
+$l['mybot_email_desc'] = "Please enter the e-mail for the new User";
+$l['mybot_email_not'] = "No e-mail given";
+$l['mybot_group'] = "Group";
+$l['mybot_group_desc'] = "To which group should the new User belong?";
+$l['mybot_group_not'] = "internal error [2]";
+$l['mybot_groupname'] = "Groupname";
+$l['mybot_groupname_desc'] = "How should the new group called?";
+$l['mybot_groupname_not'] = "No groupname given";
+$l['mybot_save'] = "Save";
+$l['mybot_installed_group'] = "{1}<br />user \"{2}\" and group \"{3}\" added<br />Please edit this again";
+$l['mybot_installed_user'] = "{1}<br />user \"{2}\" added<br />Please edit this again";
+$l['mybot_installed'] = "MyBot has been successfully installed.";
+
+$l['mybot_pl_missing'] = "The plugin depends on <a href='http://mods.mybb.com/view/pluginlibrary'>PluginLibrary</a>, which is missing. Please install it";
+$l['mybot_pl_old'] = "The plugin depends on <a href='http://mods.mybb.com/view/pluginlibrary'>PluginLibrary</a>, which is too old. Please install a newer version (Version 8 or higher required)";
+?>
