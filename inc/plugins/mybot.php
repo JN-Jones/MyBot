7cc5a2e6ddf01655d6ca9dc834bbf9d9b655049c
JN-Jones <Jones_h1@web.de>
2012-08-13 20:12:37 +0200

    Changed last things before release (Version 1.0)

--- inc/plugins/mybot.php
+++ inc/plugins/mybot.php
@@ -22,11 +22,11 @@ function mybot_info()
 	return array(
 		"name"			=> "MyBot",
 		"description"	=> "Adds a simple Bot to your MyBB",
-		"website"		=> "",
+		"website"		=> "http://mybbdemo.tk",
 		"author"		=> "Jones",
 		"authorsite"	=> "http://mybbdemo.tk",
-		"version"		=> "1.0 Beta 2.1",
-		"guid" 			=> "",
+		"version"		=> "1.0",
+		"guid" 			=> "807812530461f05f83ac7992a83c0b41",
 		"compatibility" => "16*"
 	);
 }
